237648fdcf4007974f91f8c3ffdcf332f2d6284c
정민우 <meanwo1017@gmail.com>
2021-07-19 11:27:58 +0900

    7/19 6ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,9 +2,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
+
+
 
 urlpatterns = [
     path('hello_world/', hello_world, name="hello_world"),
@@ -15,4 +17,5 @@ urlpatterns = [
 
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
 ]

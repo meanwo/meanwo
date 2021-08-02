37110c95fb2e101547e13ce4500c4d45450f33fa
정민우 <meanwo1017@gmail.com>
2021-08-02 12:42:14 +0900

    8/2 6ban class commit 5

---
+++ articleapp/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+
+from articleapp.models import Article
+
+
+class ArticleCreationsForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'image','content']
+
